0e0b2c27736480066865a38f0de93bbd79a653e8
Gin <paradoxclover66@gmail.com>
2021-02-07 17:39:47 +0530

    changes

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+  MY_API_TOKEN:
+    "pk.eyJ1IjoiY2xvdmVyMDA3IiwiYSI6ImNra2ljYjJxZDB2eTcyd3BkaWllZWxiNmEifQ.coxyohLtpS0vlCm_OKjRXg",
+  SECRET_API_KEY: "OSwuufgsvvvsbGfROvO9VVAnQ2WDg",
+};
